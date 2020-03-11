cff7abff13fe2dc96503b31997beafaa87b098cc
Warren Uhrich <hello@warren.codes>
2020-03-11 10:19:44 -0600

    Add findIndex() method.

--- js/scripts.js
+++ js/scripts.js
@@ -16,3 +16,12 @@ console.log( isMyStringInside ); // We would get the matching element.
 // Find is helpful for checking if an element exists in an array!
 var isMyStringInside = myArray.find( function ( element ) { return element === 'Goodbye, Everyone :('; } );
 console.log( isMyStringInside ); // Not found :(
+
+/**
+ * .findIndex()
+ */
+
+// Still searching for an element!
+var findAnIndex = myArray.findIndex( function ( element ) { return element === 2; } );
+console.log( findAnIndex ); // But this time, it returned the value's INDEX.
+
