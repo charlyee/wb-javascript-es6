8ac3edad0e9754e43d97432f5825713f864676e2
Warren Uhrich <hello@warren.codes>
2020-03-11 10:06:42 -0600

    Add find method.

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+/**
+ * Let's see what some of our newer array methods are...
+ */
+
+/**
+ * Find Method:
+ */
+var myArray = [ 'Hello, World!', 2, 3, true, 5, null, 38 ];
+var myValue = myArray.find( function ( element ) { return element > 4; } );
+console.log( myValue ); // Returns 5, an element from our array! Only gives us one item back.
+
+// For checking if an element exists in the array.
+var isMyStringInside = myArray.find( function ( element ) { return element === 'Hello, World!'; } );
+console.log( isMyStringInside ); // We would get the matching element.
+
+// Find is helpful for checking if an element exists in an array!
+var isMyStringInside = myArray.find( function ( element ) { return element === 'Goodbye, Everyone :('; } );
+console.log( isMyStringInside ); // Not found :(
