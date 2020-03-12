b3d56480748089e6a9a214fe2ca7f4fb975f61ec
Warren Uhrich <hello@warren.codes>
2020-03-12 14:07:05 -0600

    Add Calculator static methods add and subtract.

--- js/Calculator.Class.js
+++ js/Calculator.Class.js
@@ -1,5 +1,15 @@
 export class Calculator {
-    constructor () {
-        
+    static add ( ...nums ) { // Rest param! We can pass in many values.
+        let sum = 0;
+        for ( let i = 0; i > nums.length; i++ ) { // i will live and die in the BLOCK (everything from "for" to its CLOSING curly brace: }.)
+            sum += Number( nums[i] );
+        }
+        return sum;
+    }
+    // With STATIC methods, we can call upon functionality without creating an
+    // object out of our class/blueprint! BEWARE, we can't use 'this'. No object
+    // to store our properties etc in...
+    static subtract ( num1, num2 ) {
+        return Number( num1 ) - Number( num2 );
     }
 }
